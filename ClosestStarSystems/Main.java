093e373c95be5d9a0bcf3ad13e016d0ba2960067
Mr. Lo <55058939+mrlo-codes@users.noreply.github.com>
2024-03-07 10:01:48 -0500

    Completed Closest Star Systems

--- ClosestStarSystems/Main.java
+++ ClosestStarSystems/Main.java
@@ -17,7 +17,7 @@ public class Main
 
         //Try and read in the numbers from input.txt
         try {
-            Scanner input = new Scanner(new File("ClosestStarSystems/testinput.txt"));
+            Scanner input = new Scanner(new File("ClosestStarSystems/puzzledata.txt"));
             while(input.hasNextLine()){
                 in.add(input.nextLine());
             }
@@ -34,33 +34,72 @@ public class Main
 
     public static void test(){
         ArrayList<String[]> stars = new ArrayList<String[]>();
+        ArrayList<Double> dist = new ArrayList<Double>();
         String[] currStar = new String[5];
+        String tempString = "";
+        String[] temp;
 
         int posDist, posX, posY, posZ;
         String currText = "";
 
 
         for(String s : in){
-            
-            /*posZ = s.lastIndexOf(" ") + 1;
-            posY = s.lastIndexOf(" ", posZ) + 1;
-            posX = s.lastIndexOf(" ", posY) + 1;
-            posDist = s.lastIndexOf(" ", posX) + 1;
+            temp = s.split("\\s+");
+            //System.out.println(Arrays.toString(temp));
+            for(int i = temp.length - 1; i >= 0; i--){
+                //System.out.println(i);
+                if(i == temp.length - 1){
+                    currStar[4] = temp[i];
+                } else if (i == temp.length - 2){
+                    currStar[3] = temp[i];
+                } else if (i == temp.length - 3){
+                    currStar[2] = temp[i];
+                } else if (i == temp.length - 4){
+                    currStar[1] = temp[i];
+                } else {
+                    currText = temp[i] + " " + currText;
+                }
+            }
 
-            System.out.println(s);
-            System.out.println(posDist + " " + posX + " " + posY + " " + posZ);
-            System.out.println(s.length());
-            */
+            currStar[0] = currText;
 
-            //currName = s.substring(0, s.indexOf(" "));
-            //currDist = s.substring(posDist, posX - 1);
-            //currX = s.substring(posX, posY - 1);
-            //currY = s.substring(posY, posZ - 1);
-            //currZ = s.substring(posZ);
+            //System.out.println(Arrays.toString(currStar));
+
+            stars.add(currStar);
+
+            currStar = new String[5];
+            currText = "";
+
         }
 
-        
-        //System.out.println(currName + " " + currDist + " " + currX + " " + currY + " " + currZ);
+        //Process the distances
+        stars.remove(0);
+            
+        double x1, y1, z1, x2, y2, z2;
+
+        for(int i = 0; i < stars.size(); i++){
+            for(int j = i + 1; j < stars.size(); j++){
+                x1 = Double.parseDouble(stars.get(i)[2]);
+                y1 = Double.parseDouble(stars.get(i)[3]);
+                z1 = Double.parseDouble(stars.get(i)[4]);
+                x2 = Double.parseDouble(stars.get(j)[2]);
+                y2 = Double.parseDouble(stars.get(j)[3]);
+                z2 = Double.parseDouble(stars.get(j)[4]);
+
+                dist.add(pythagoreas(x1, y1, z1, x2, y2, z2));
+            }
+        }
+
+        //System.out.println(dist);
+        double smallest = dist.get(0);
+
+        for(double d : dist){
+            if(d < smallest){
+                smallest = d;
+            }
+        }
+
+        System.out.println("Smallest is: " + smallest);
     
     }
 
