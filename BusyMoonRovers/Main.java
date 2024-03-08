1756ac87be320c2387082b00bd42ea1e7fd91ced
Mr. Lo <clo@briarcliffschools.org>
2024-03-08 08:15:53 -0500

    Still working on it...

--- BusyMoonRovers/Main.java
+++ BusyMoonRovers/Main.java
@@ -17,7 +17,7 @@ public class Main
 
         //Try and read in the numbers from input.txt
         try {
-            Scanner input = new Scanner(new File("ClosestStarSystems/testinput.txt"));
+            Scanner input = new Scanner(new File("BusyMoonRovers/testinput.txt"));
             while(input.hasNextLine()){
                 in.add(input.nextLine());
             }
@@ -35,11 +35,52 @@ public class Main
     public static void test(){
         String start, dest;
 
-        ArrayList<String[]> distances = new ArrayList<String]>;
+        ArrayList<String[]> distances = new ArrayList<String[]>();
+        String[] curr;
 
-        
+        //Process the inputs of base distances into an organized structure
+        for(int i = 0; i < in.size(); i++){
+            if(!in.get(i).equals("")){
+                curr = in.get(i).split("\\s+");
 
-    
+                if(i == 0){
+                    String[] temp = new String[curr.length + 1];
+                    temp[0] = "";
+
+                    int counter = 1;
+
+                    for(String s : curr){
+                        temp[counter] = s;
+                        counter++;
+                    }
+
+                    curr = temp;
+                }
+
+            } else {
+                break;
+            }
+            /* 
+            //Debugging
+            for(int j = 0; j < curr.length; j++){
+                System.out.print(curr[j] + " " );
+            }
+            System.out.println();
+            */
+        }
+
+        //Process the route
+        String[] route;
+
+        int routeIndex = in.indexOf("") + 1;
+
+        //System.out.println(in.get(routeIndex));
+
+        String routes = in.get(routeIndex);
+        routes = routes.substring(routes.indexOf(":") + 2);
+        route = routes.split("->");
+
+        System.out.println(routes);
     }
 
     
