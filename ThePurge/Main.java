81e77c6db0ced6b6390e633d2aba2f5318cd7793
Mr. Lo <55058939+mrlo-codes@users.noreply.github.com>
2024-03-14 16:29:31 -0400

    Still working on ThePurge

--- ThePurge/Main.java
+++ ThePurge/Main.java
@@ -37,7 +37,42 @@ public class Main
     public static void test(){
         processFiles();
         folders.remove(0);
-        printDirectory();
+        
+
+        ArrayList<String[]> currFolder;
+        String[] currFile;
+        int sumOfBytesDeleted = 0;
+
+        for(int i = 0; i < folders.size(); i++){
+            currFolder = folders.get(i);
+            System.out.println("Going through folder " + i + " now with " + currFolder.size() + " items");
+
+            for(int j = 0; j < currFolder.size(); j++){
+                System.out.println("running...");
+                currFile = currFolder.get(j);
+
+                if(currFile[0].contains("delete") || currFile[0].contains("temporary")){
+                    //check if it is a file or a folder
+                    System.out.println("Processing..." + currFile[0] + " with file info " + currFile[1]);
+                    if(currFile[1].indexOf("FOLDER") != -1){
+                        int folderNum = Integer.parseInt(currFile[1].substring(8, 9));
+                        System.out.println("Getting ready to delete Folder" + folderNum);
+                        sumOfBytesDeleted+= removeDirectory(folderNum);
+                    } else {
+                        sumOfBytesDeleted+= Integer.parseInt(currFile[1]);
+                        currFolder.remove(j);
+                        j--;
+                    }
+                }
+
+            }
+
+        }
+
+        //printDirectory();
+
+        System.out.println("Number of bytes deleted: " + sumOfBytesDeleted);
+
 
     }
 
@@ -51,14 +86,18 @@ public class Main
                 folders.add(folder);
                 folder = new ArrayList<String[]>();
             } else {
-                file[0] = s.substring(s.indexOf("-") + 2, s.lastIndexOf(" "));
-                file[1] = s.substring(s.lastIndexOf(" ") + 1);
+                file[0] = s.substring(s.indexOf("-") + 2, s.indexOf(" ", 3));
+                file[1] = s.substring(s.indexOf(" ", 3) + 1);
+
+                //System.out.println(file[0] + ", " + file[1]);
 
                 folder.add(file);
 
                 file = new String[2];
             }
         }
+
+        folders.add(folder);
 
     }
 
@@ -80,7 +119,34 @@ public class Main
 
     }
 
-    
+    public static int removeDirectory(int dir){
+        ArrayList<String[]> currFolder = folders.get(dir);
+        String[] currFile;
+        int sumOfBytesDeleted = 0;
+
+        //Go through everything in the folder and add the bytes
+        for(int j = 0; j < currFolder.size(); j++){
+            currFile = currFolder.get(0);
+
+
+            //check if it is a file or a folder
+            if(currFile[1].contains("FOLDER")){
+                int folderNum = Integer.parseInt(currFile[1].substring(8, 9));
+                System.out.println("\tGetting ready to delete Folder " + folderNum);
+                sumOfBytesDeleted+= removeDirectory(folderNum);
+            } else {
+                sumOfBytesDeleted+= Integer.parseInt(currFile[1]);
+                currFolder.remove(j);
+                j--;
+            }
+            
+        }
+
+        System.out.println("removed Folder " + dir);
+
+        folders.remove(dir);
+        return sumOfBytesDeleted;
+    }
 
     
 }
