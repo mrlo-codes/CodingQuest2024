797a09b287a514b5f34e2301a190133a95ae192d
Mr. Lo <55058939+mrlo-codes@users.noreply.github.com>
2024-03-15 10:33:41 -0400

    Starting ConnectingCities

---
+++ ConnectingCities/Main.java
@@ -0,0 +1,32 @@
+package ConnectingCities;
+import java.util.*;
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author Lo
+ * @version 3-15-24
+ */
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+
+
+        test();
+
+    }
+
+    public static void test(){
+       
+
+
+    }
+
+    public static void actual(){
+    }
+
+    
+    
+}
